8d3a870b2b6793a95f4ec3ac69733d909fab5ba9
galeriqbal <galeriqbal@gmail.com>
2026-07-30 23:54:06 +0700

    update schema

---
+++ revoshop_db/schema.sql
@@ -0,0 +1,35 @@
+create table users(
+id serial primary key,
+name varchar(100) not null,
+email varchar(100) not null unique,
+password_hash varchar(255) not null,
+created_at timestamptz default now());
+
+create table categories(
+id serial primary key,
+name varchar(100) not null,
+created_at timestamptz default now());
+
+create table products(
+id serial primary key,
+categories_id integer references categories(id),
+name varchar(100) not null,
+price  numeric (12,2),
+stock integer not null,
+created_at timestamptz default now());
+
+create table orders(
+id serial primary key,
+user_id integer references users(id),
+total_prices numeric (12,2),
+created_at timestamptz default now());
+
+create table  order_items(
+order_id integer not null,
+product_id integer not null,
+quantity integer not null check (quantity > 0),
+
+primary key (order_id, product_id),
+
+constraint fk_order_id foreign key (order_id) references orders(id) on delete cascade,
+constraint fk_products_id foreign key (product_id) references products(id));

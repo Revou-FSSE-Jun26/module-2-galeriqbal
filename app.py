5d3da2cdccfdea1bc601e35ef354cc03dbc67e5f
galeriqbal <galeriqbal@gmail.com>
2026-08-12 18:43:42 +0700

    update flask dan alchemysql setting

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# TODO 1: Set SQLALCHEMY_DATABASE_URI to connect to your local PostgreSQL 'store_db'
+# Format: postgresql://username:password@host/database_name
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123789@localhost/revoshop_db'
+
+# TODO 2: Set SQLALCHEMY_TRACK_MODIFICATIONS to False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# TODO 3: Initialize SQLAlchemy with the app
+db = app
+
+@app.route('/')
+def index():
+    return jsonify({"message": "Flask is connected to PostgreSQL!", "status": "ok"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
